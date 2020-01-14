a4c9185d282e7dff4f7542b42be9b8d62d8d5e59
fauez <fauez.passos@inpe.br>
2020-01-14 14:04:26 -0300

    create index route in DevController

---
+++ src/controllers/DevController.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+import Dev from '../models/Dev';
+
+class DevController {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  }
+
+  async store(req, res) {
+    const { github_username, techs, latitude, longitude } = req.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      // eslint-disable-next-line no-undef
+      const { name = login, avatar_url, bio } = apiResponse.data;
+
+      const techsArrays = techs.split(',').map(tech => tech.trim());
+
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
+
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArrays,
+        location,
+      });
+    }
+
+    return res.json(dev);
+  }
+}
+
+export default new DevController();
--- src/routes.js
+++ src/routes.js
@@ -1,35 +1,10 @@
 import { Router } from 'express';
-import axios from 'axios';
-import Dev from './models/Dev';
+
+import DevController from './controllers/DevController';
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs, latitude, longitude } = req.body;
-
-  const apiResponse = await axios.get(
-    `https://api.github.com/users/${github_username}`
-  );
-
-  const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArrays = techs.split(',').map(tech => tech.trim());
-
-  const location = {
-    type: 'Point',
-    coordinates: [longitude, latitude],
-  };
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArrays,
-    location,
-  });
-
-  return res.json(dev);
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
 export default routes;
