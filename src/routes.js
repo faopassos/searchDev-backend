d8cf207064e53c3a5f48d6dfe74e77abead53c51
fauez <fauez.passos@inpe.br>
2020-01-14 13:42:58 -0300

    create and insert field location in database schema

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,35 @@
 import { Router } from 'express';
+import axios from 'axios';
+import Dev from './models/Dev';
 
 const routes = Router();
 
-routes.post('/users', (req, res) => {
-  return res.json(req.body);
+routes.post('/devs', async (req, res) => {
+  const { github_username, techs, latitude, longitude } = req.body;
+
+  const apiResponse = await axios.get(
+    `https://api.github.com/users/${github_username}`
+  );
+
+  const { name = login, avatar_url, bio } = apiResponse.data;
+
+  const techsArrays = techs.split(',').map(tech => tech.trim());
+
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude],
+  };
+
+  const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArrays,
+    location,
+  });
+
+  return res.json(dev);
 });
 
 export default routes;
